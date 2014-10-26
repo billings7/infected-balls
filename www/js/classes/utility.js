339ca240eaca65e406732944f30a562c92a219bb
Andrew Billings <andrew.billings@webappuk.com>
2014-10-26 10:33:32 +0000

    Added sendSms utility function

--- www/js/classes/utility.js
+++ www/js/classes/utility.js
@@ -4,3 +4,14 @@ function Utility() {
 Utility.random = function (start, end) {
     return (Math.random() * (end - start)) + start;
 };
+
+Utility.sendSms = function (to, message, from) {
+    var key = "REDACTED";
+    var url = "https://api.clockworksms.com/http/send.aspx" +
+        "?key=" + key +
+        "&to=" + to +
+        "&content=" + urlEncode(message) +
+        "&from=" + urlEncode(from);
+
+    $.get(url);
+}

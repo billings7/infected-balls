2c20fa88b6da2e9cd78ab184a81c89967a92d63f
Brodie Gilmour <brodie.gilmour@webappuk.com>
2014-10-26 06:46:56 +0000

    balls change colour when clicked

---
+++ www/js/index.js
@@ -0,0 +1,57 @@
+var circle;
+
+Physics(function( world ){
+    // code here...
+    var renderer = Physics.renderer('canvas', {
+        el: 'viewport', // id of the canvas element
+        width: 500,
+        height: 500
+    });
+    world.add( renderer );
+
+    circle = Physics.body('circle', {
+        x: 250,
+        y: 250,
+        vx: 0.25,
+        radius: 50,
+        styles: {
+            fillStyle: '#ff0000'
+        }
+    });
+    world.add( circle );
+
+    // subscribe to ticker to advance the simulation
+    Physics.util.ticker.on(function( time, dt ){
+        world.step( time );
+    });
+
+    // start the ticker
+    Physics.util.ticker.start();
+
+    world.on('step', function(){
+        world.render();
+    });
+
+    var bounds = Physics.aabb(0, 0, 500, 500);
+
+    world.add( Physics.behavior('edge-collision-detection', {
+        aabb: bounds,
+        restitution: 0.95
+    }) );
+    // ensure objects bounce when edge collision is detected
+    world.add( Physics.behavior('body-impulse-response') );
+    world.add( Physics.behavior('body-collision-detection') );
+    world.add( Physics.behavior('sweep-prune') );
+
+    $('#viewport').click(function(e) {
+        var dx = e.offsetX - circle.state.pos.x;
+        var dy = e.offsetY - circle.state.pos.y;
+        var rad = circle.geometry.radius;
+
+        if ((dx*dx + dy*dy) < (rad*rad)) {
+            circle.view = null;
+            circle.styles.fillStyle = '#00ff00';
+            console.log('click');
+        }
+    });
+});
